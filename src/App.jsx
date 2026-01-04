a607336727cf682ebabb7510e0f6a0c0170633ed
Noumouké <212020060+Noum-Souare@users.noreply.github.com>
2026-01-05 00:26:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2312,7 +2312,7 @@ function App() {
                 <div className="service-icon">⛓️</div>
                 <strong>Blockchain</strong>
               </div>
-              <div className="floating-card">
+              <div className="floating-card" style={{padding: '0.75rem', maxWidth: '180px'}}>
                 <div className="service-icon">🛡️</div>
                 <strong>Gouvernance des données & Accompagnement dans la conformité</strong>
               </div>
